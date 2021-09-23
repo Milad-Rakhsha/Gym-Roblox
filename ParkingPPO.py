e19724f54ebd4fec506ab8915275f2145a2842e9
DESKTOP-QMNCFGL\Milad <rakhsha@wisc.edu>
2021-09-23 09:21:27 -0700

    Latest parking example results

--- ParkingPPO.py
+++ ParkingPPO.py
@@ -21,21 +21,21 @@ def copyCurrentSource(log_dir):
     copyfile("ParkingPPO.py", log_dir + "/" + str(max_run+1) + ".py")
 
 
-
+portNumber=8080
 # Create log dir
 log_dir = "./Parking-PPO/"
 os.makedirs(log_dir, exist_ok=True)
-env=VecMonitor(RobloxVecEnv(8080), log_dir)
+env=VecMonitor(RobloxVecEnv(portNumber), log_dir)
 env = VecNormalize(env, norm_obs=True, norm_reward=True,
                    clip_obs=1.)
 
 max_episode_length=env.info["timeout"]
 num_envs = env.num_envs
-n_steps = 10 # max_episode_length//10
+n_steps = 20 # max_episode_length//10
 rollout = num_envs * n_steps
 batch_size = rollout
 n_epochs= 10 #rollout//batch_size * 5
-log_interval= max(max_episode_length//n_steps//50,1)
+log_interval= max(max_episode_length//n_steps//25,1)
 
 model = PPO('MlpPolicy', env=env,
             verbose=True,
@@ -53,18 +53,11 @@ model = PPO('MlpPolicy', env=env,
             )
 
 checkpoint_callback = CheckpointCallback(save_freq=5000, save_path=log_dir,
-                                         name_prefix='rl_model')
+                                         name_prefix='rl_model_%s'%(portNumber))
 print("max_episode_length=%d, n_steps=%d, rollout=%d, batch_size=%d, n_epochs=%d"%(max_episode_length, n_steps, rollout, batch_size, n_epochs))
 
 #If we need to change any model parameter we should do it after the load process
-# model = PPO.load(log_dir + "/rl_model_2000000_steps", verbose=True, tensorboard_log=log_dir, env=env)
-# model.n_steps = n_steps
-# model.rollout = rollout
-# model.batch_size = batch_size
-# model.n_epochs= n_epochs
-# model.log_interval= log_interval
-# model.learning_rate=0.0001
-# model.gae_lambda=0.7
+# model = PPO.load(log_dir + "/rl_model_8060_12000000_steps", verbose=True, tensorboard_log=log_dir, env=env)
 
 copyCurrentSource(log_dir)
 with ProgressBarManager(5e7) as ProgressBar: # this the garanties that the tqdm progress bar closes correctly
@@ -72,5 +65,5 @@ with ProgressBarManager(5e7) as ProgressBar: # this the garanties that the tqdm 
 model.save(log_dir + "/Final")
 
 # Evaluate the trained agent
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=20)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=1)
 print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
